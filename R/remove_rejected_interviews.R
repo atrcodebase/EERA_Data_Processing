79362136e5e6148eb784e715e0796d5b5e3fbc04
shayantaqi <taqi.shayan@gmail.com>
2023-12-18 15:40:09 +0430

    Fix the Grades Curriculum Deletion keys

--- R/remove_rejected_interviews.R
+++ R/remove_rejected_interviews.R
@@ -9,7 +9,7 @@ clean_data.tool1$Other_Shifts_Detail <- clean_data.tool1$Other_Shifts_Detail %>%
 clean_data.tool1$Headmasters <- clean_data.tool1$Headmasters %>% filter(PARENT_KEY %in% clean_data.tool1$data$KEY & !KEY %in% deleted_keys_ps)
 clean_data.tool1$Curriculum_Changes <- clean_data.tool1$Curriculum_Changes %>% filter(PARENT_KEY %in% clean_data.tool1$data$KEY & !KEY %in% deleted_keys_ps)
 clean_data.tool1$Weekly_Class_Schedule <- clean_data.tool1$Weekly_Class_Schedule %>% filter(PARENT_KEY %in% clean_data.tool1$Curriculum_Changes$KEY & !KEY %in% deleted_keys_ps)
-clean_data.tool1$Grades_Curriculum <- clean_data.tool1$Grades_Curriculum %>% filter(PARENT_KEY %in% clean_data.tool1$data$KEY & !KEY %in% deleted_keys_ps)
+clean_data.tool1$Grades_Curriculum <- clean_data.tool1$Grades_Curriculum %>% filter(PARENT_KEY %in% clean_data.tool1$Curriculum_Changes$KEY & !KEY %in% deleted_keys_ps)
 clean_data.tool1$Subjects_Detail <- clean_data.tool1$Subjects_Detail %>% filter(PARENT_KEY %in% clean_data.tool1$data$KEY & !KEY %in% deleted_keys_ps)
 clean_data.tool1$Education_Quality <- clean_data.tool1$Education_Quality %>% filter(PARENT_KEY %in% clean_data.tool1$data$KEY & !KEY %in% deleted_keys_ps)
 clean_data.tool1$Relevant_photos <- clean_data.tool1$Relevant_photos %>% filter(PARENT_KEY %in% clean_data.tool1$data$KEY & !KEY %in% deleted_keys_ps)
