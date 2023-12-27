c1a5434aed9f148d30f12fba106edd21d6bf0e40
shayantaqi <taqi.shayan@gmail.com>
2023-12-27 14:58:39 +0430

    Added Data Entry Tool to ouliter module

--- R/check_outliers.R
+++ R/check_outliers.R
@@ -3,6 +3,7 @@ if(!require(cleaninginspectoR)) devtools::install_github("ellieallien/cleaningin
 to_be_ignored_vars <- c("D7_Year", "G4_Minutes")
 
 # extracting the numeric indicators of each tool
+tool0.numeric_vars <- kobo_tool.tool0$survey |> filter(type == "integer" & !name %in% to_be_ignored_vars) |> pull(name)
 tool1.numeric_vars <- kobo_tool.tool1$survey |> filter(type == "integer" & !name %in% to_be_ignored_vars) |> pull(name)
 tool2.numeric_vars <- kobo_tool.tool2$survey |> filter(type == "integer" & !name %in% to_be_ignored_vars) |> pull(name)
 tool3.numeric_vars <- kobo_tool.tool3$survey |> filter(type == "integer" & !name %in% to_be_ignored_vars) |> pull(name)
@@ -15,6 +16,18 @@ tool9.numeric_vars <- kobo_tool.tool9$survey |> filter(type == "integer" & !name
 
 
 # creating the indices to facilitate joining of meta cols and KEY ---------
+meta_cols_tool0 <- clean_data.tool0$data %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
+meta_cols_tool0.Tool3_Grades_Repeat <- clean_data.tool0$Tool3_Grades_Repeat %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
+meta_cols_tool0.Tool3_Class_Attendance <- clean_data.tool0$Tool3_Class_Attendance %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
+meta_cols_tool0.Tool3_T3_N_Classes_Repeat <- clean_data.tool0$Tool3_T3_N_Classes_Repeat %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
+meta_cols_tool0.Tool3_Headcount <- clean_data.tool0$Tool3_Headcount %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
+meta_cols_tool0.Tool1_Timetable_Year <- clean_data.tool0$Tool1_Timetable_Year %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
+meta_cols_tool0.Tool1_Timetable1_Repeat <- clean_data.tool0$Tool1_Timetable1_Repeat %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY) # Under Tool1_Timetable_Year
+meta_cols_tool0.Tool1_Timetable2_Repeat <- clean_data.tool0$Tool1_Timetable2_Repeat %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY) # Under Tool1_Timetable_Year
+meta_cols_tool0.Tool1_Timetable3_Repeat <- clean_data.tool0$Tool1_Timetable3_Repeat %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY) # Under Tool1_Timetable_Year
+meta_cols_tool0.Tool1_Timetable4_Repeat <- clean_data.tool0$Tool1_Timetable4_Repeat %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY) # Under Tool1_Timetable_Year
+
+
 meta_cols_tool1 <- clean_data.tool1$data %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
 meta_cols_tool1.School_Operationality <- clean_data.tool1$School_Operationality %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
 meta_cols_tool1.Shifts_Detail <- clean_data.tool1$Shifts_Detail  %>% mutate(index = as.numeric(row.names(.))) |> select(index, all_of(unname(meta_cols)), KEY)
@@ -64,6 +77,23 @@ meta_cols_tool9 <- clean_data.tool9$data %>% mutate(index = as.numeric(row.names
 
 # logging the outliers ----------------------------------------------------
 outlier_check_result <- rbind(
+  # Tool 0 -- Confirm values for 6666
+  rbind(
+    find_outliers(clean_data.tool0$data) |> left_join(meta_cols_tool0, by = "index") |> mutate(Tab_name = "data"),
+    find_outliers(clean_data.tool0$Tool3_Grades_Repeat) |> left_join(meta_cols_tool0.Tool3_Grades_Repeat, by = "index") |> mutate(Tab_name = "Tool3_Grades_Repeat"),
+    find_outliers(clean_data.tool0$Tool3_Class_Attendance) |> left_join(meta_cols_tool0.Tool3_Class_Attendance, by = "index") |> mutate(Tab_name = "Tool3_Class_Attendance"),
+    find_outliers(clean_data.tool0$Tool3_T3_N_Classes_Repeat) |> left_join(meta_cols_tool0.Tool3_T3_N_Classes_Repeat, by = "index") |> mutate(Tab_name = "Tool3_T3_N_Classes_Repeat"),
+    find_outliers(clean_data.tool0$Tool3_Headcount) |> left_join(meta_cols_tool0.Tool3_Headcount, by = "index") |> mutate(Tab_name = "Tool3_Headcount"),
+    find_outliers(clean_data.tool0$Tool1_Timetable_Year) |> left_join(meta_cols_tool0.Tool1_Timetable_Year, by = "index") |> mutate(Tab_name = "Tool1_Timetable_Year"),
+    find_outliers(clean_data.tool0$Tool1_Timetable1_Repeat) |> left_join(meta_cols_tool0.Tool1_Timetable1_Repeat, by = "index") |> mutate(Tab_name = "Tool1_Timetable1_Repeat"),
+    find_outliers(clean_data.tool0$Tool1_Timetable2_Repeat) |> left_join(meta_cols_tool0.Tool1_Timetable2_Repeat, by = "index") |> mutate(Tab_name = "Tool1_Timetable2_Repeat"),
+    find_outliers(clean_data.tool0$Tool1_Timetable3_Repeat) |> left_join(meta_cols_tool0.Tool1_Timetable3_Repeat, by = "index") |> mutate(Tab_name = "Tool1_Timetable3_Repeat"),
+    find_outliers(clean_data.tool0$Tool1_Timetable4_Repeat) |> left_join(meta_cols_tool0.Tool1_Timetable4_Repeat, by = "index") |> mutate(Tab_name = "Tool1_Timetable4_Repeat")
+  ) |> 
+    filter(variable %in% tool0.numeric_vars) |> 
+    mutate(tool = "Tool Data Entry") |> 
+    select(KEY,all_of(meta_cols), index, question_name = variable, old_value = value, tool, Tab_name) |> filter(old_value != 6666),
+  
   # Tool 1
   rbind(
     find_outliers(clean_data.tool1$data) |> left_join(meta_cols_tool1, by = "index") |> mutate(Tab_name = "data"),
